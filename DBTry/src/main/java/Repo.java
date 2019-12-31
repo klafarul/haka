e980a0fed7699783aba52a2ffa3d5cb642954e4d
klafarul <41956123+klafarul@users.noreply.github.com>
2019-12-31 10:36:05 +0300

    deleting files

--- DBTry/src/main/java/Repo.java
+++ DBTry/src/main/java/Repo.java
@@ -1,4 +1,17 @@
-package PACKAGE_NAME;
+import java.util.ArrayList;
 
 public class Repo {
+    ArrayList<Integer> intRepo;
+    private static Repo repo;
+    private Repo(){
+        intRepo = new ArrayList<Integer>();
+    }
+
+    public static Repo getRepo(){
+        if (repo == null){
+            repo = new Repo();
+        }
+        return repo;
+    }
+
 }
